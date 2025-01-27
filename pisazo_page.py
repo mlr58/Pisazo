8b7545b475b1ec835557692cdce388eab9414523
mlr58 <166216437+mlr58@users.noreply.github.com>
2025-01-27 17:09:29 +0100

    Update pisazo_page.py

--- pisazo_page.py
+++ pisazo_page.py
@@ -160,67 +160,65 @@ def main():
     d1.table(debts)
     d2.pyplot(fig)
 
-    try:
-        url = 'https://scu.ugr.es'
-        page = urlopen(url)
-        html_bytes = page.read()
-        html = html_bytes.decode("utf-8")
-        text = html.split('\n')
-    
-        words = ['  DE  ', 'Menú 1', 'Menú 2', 'Primero', 'Segundo', 'Acompañamiento', 'Postre', 'Cremas y sopas']
-    
-        for i in reversed(range(len(text))):
-            line = text[i]
-            if not any([x in line for x in words]):
-                text.pop(i)
-            else:
-                text[i] = line.replace('\t', '').replace('<th class="leftalign" colspan="2"><strong>', '')\
-                    .replace('</strong>  </th><th class="rightalign">  </th>', '')\
-                    .replace('<td colspan="2"><strong>', '')\
-                    .replace('</strong> </td><td><strong><em>Alérgenos</em></strong></td>', '')\
-                    .replace('<td class="leftalign">', '')\
-                    .replace('  </td><td class="leftalign"><strong>', '')\
-                    .replace('</strong>  </th><th class="rightalign">  </th>', '')\
-                    .replace('  </td><strong>', ': ')\
-                    .replace('  ', ' ')
-                text[i] = text[i].split('</strong>')[0]
-    
-        today = datetime.today().day
-    
-        for i in range(len(text)):
-            if text[i] == text[0] and i != 0:
-                text = text[0:i]
-                break
-    
-        for i in range(len(text)):
-            line = text[i]
-            if f"{today+1} DE" in line:
-                text = text[0:i]
-                break
-    
-        for i in range(len(text)):
-            line = text[i]
-            if f"{today} DE" in line:
-                text = text[i:]
-                break
-        text = text[1:]
-        for i in range(len(text)):
-            try:
-                text[i] = text[i].split(': ')[1]
-            except:
-                pass
-        menu1 = text[:int(len(text)/2)]
-        menu2 = text[int(len(text)/2):]
-    
-        df = pd.DataFrame(columns=[x[0] for x in [menu1, menu2]])
-        df[menu1[0]] = menu1[1:]
-        df[menu2[0]] = menu2[1:]
-    
-        with st.sidebar:
-            st.markdown(df.style.hide(axis="index").to_html(), unsafe_allow_html=True)
 
-    except:
-        pass
+    url = 'https://scu.ugr.es'
+    page = urlopen(url)
+    html_bytes = page.read()
+    html = html_bytes.decode("utf-8")
+    text = html.split('\n')
+
+    words = ['  DE  ', 'Menú 1', 'Menú 2', 'Primero', 'Segundo', 'Acompañamiento', 'Postre', 'Cremas y sopas']
+
+    for i in reversed(range(len(text))):
+        line = text[i]
+        if not any([x in line for x in words]):
+            text.pop(i)
+        else:
+            text[i] = line.replace('\t', '').replace('<th class="leftalign" colspan="2"><strong>', '')\
+                .replace('</strong>  </th><th class="rightalign">  </th>', '')\
+                .replace('<td colspan="2"><strong>', '')\
+                .replace('</strong> </td><td><strong><em>Alérgenos</em></strong></td>', '')\
+                .replace('<td class="leftalign">', '')\
+                .replace('  </td><td class="leftalign"><strong>', '')\
+                .replace('</strong>  </th><th class="rightalign">  </th>', '')\
+                .replace('  </td><strong>', ': ')\
+                .replace('  ', ' ')
+            text[i] = text[i].split('</strong>')[0]
+
+    today = datetime.today().day
+
+    for i in range(len(text)):
+        if text[i] == text[0] and i != 0:
+            text = text[0:i]
+            break
+
+    for i in range(len(text)):
+        line = text[i]
+        if f"{today+1} DE" in line:
+            text = text[0:i]
+            break
+
+    for i in range(len(text)):
+        line = text[i]
+        if f"{today} DE" in line:
+            text = text[i:]
+            break
+    text = text[1:]
+    for i in range(len(text)):
+        try:
+            text[i] = text[i].split(': ')[1]
+        except:
+            pass
+    menu1 = text[:int(len(text)/2)]
+    menu2 = text[int(len(text)/2):]
+
+    df = pd.DataFrame(columns=[x[0] for x in [menu1, menu2]])
+    df[menu1[0]] = menu1[1:]
+    df[menu2[0]] = menu2[1:]
+
+    with st.sidebar:
+        st.markdown(df.style.hide(axis="index").to_html(), unsafe_allow_html=True)
+
 
 
 
